8706890b322928c6f25686b92bd174fba0127621
huanxing <2259924865.qq.com>
2017-04-24 15:02:07 +0800

    c8

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,6 @@ class Product < ActiveRecord::Base
   validates :price,:title,:description, presence:true
   validates :price, numericality:{greater_than_or_equal_to:0.01}
   validates :title, uniqueness:true
+  #validates :title, length:{minimum:5,maximum:10}
+  #validates_length_of :title, maximum:10	#验证字符长度
 end
