2f4ac5f3a69f388721a2206f7f8728410be5175e
huanxing <2259924865.qq.com>
2017-04-24 10:38:24 +0800

    bookstore Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookstore::Application.config.secret_token = '74bd2207de129ef6630b48d3aa2b5dc7570a3b38a49a1603ba18f8b933fc2d47108d3714650446a4f7f8fca351baae5ae7f5d8551eac153a728c95578e799da4'
