8ed31060bc140ebf2cf0cb26992595d217072265
huanxing <2259924865.qq.com>
2017-04-24 16:15:01 +0800

    c8

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -4,5 +4,7 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '.left a', minimum:2
+    assert_select '.entry', minimum:2
   end
 end
