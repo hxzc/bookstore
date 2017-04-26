1d4e64ac79c61424374e39015e7f141e43196cc9
huanxing <2259924865.qq.com>
2017-04-26 09:28:49 +0800

    prac 9.4

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -40,10 +40,10 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-  @cart = current_cart
-  product = Product.find(params[:product_id])
+    @cart = current_cart
+    product = Product.find(params[:product_id])
     @line_item = @cart.line_items.build(product: product)
-
+    session[:counter] = 0
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item.cart, 
