913a544ae0a2a9c4a4a9976525f4c8215a611d66
huanxing <2259924865.qq.com>
2017-05-04 17:31:53 +0800

    c14.4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
+#coding:utf-8
 class ApplicationController < ActionController::Base
+  before_filter :authorize
   protect_from_forgery
- 
   private
     def current_cart
       Cart.find(session[:cart_id])
@@ -9,4 +10,11 @@ class ApplicationController < ActionController::Base
 	  session[:cart_id] = cart.id
 	  cart
 	end
+
+  protected
+    def authorize
+      unless User.find_by_id(session[:user_id])
+      	redirect_to login_url, notice:"请登陆"
+      end
+    end
 end
